1256cf9df012cc5ded9ee85b82d8433701e2ce19
Juan Bourbotte <juanmbs2@gmail.com>
2025-08-11 20:37:09 -0300

    Subiendo archivo de python

---
+++ func.py
@@ -0,0 +1,23 @@
+
+def datos():
+    sexo = input("Por favor ingrese su sexo (hombre/mujer): ").lower()
+    if sexo == "hombre":
+        edad = int(input("Ingrese su edad: "))
+        if edad >= 60:
+            print("Ingreso permitido")
+            print("Ingrese sus datos personales para acceder a su cuenta")
+            nombre = input("Nombre: ")
+            apellido = input("Apellido: ")
+            dni = input("DNI: ")
+            domicilio = input("Domicilio: ")
+            print(f"Datos guardados: {nombre} {apellido}, DNI: {dni}, Domicilio: {domicilio}")
+            print("Bienvenido al PAMI señor jubilado")
+        else:
+            print("Ingreso no permitido, debe ser mayor o igual a 60 años")
+    elif sexo == "mujer":
+        print("No esta permitido sexo femenino en el equipo masculino")
+    else:
+        print("Sexo no reconocido. Por favor ingrese 'hombre' o 'mujer'.")
+datos()
+
+
